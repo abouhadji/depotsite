3d0e2ea1b3d4561dd89b70884577443160f6a387
abouhadji <64889873+abouhadji@users.noreply.github.com>
2020-09-27 16:50:31 +0200

    Add files via upload

---
+++ tt.js
@@ -0,0 +1,32 @@
+ let pseudo = 'ronaldo';
+ let tableau = [{ nom: 'ok', prix: 10 },
+     { nom: 'evi', prix: 20 },
+     { nom: 'plpl', prix: 30 },
+     { nom: 'popo', prix: 40 },
+     { nom: pseudo, prix: 50 },
+ ]
+
+
+ // function afficheRecommandations() {
+ //     console.log[0], [3];
+ // }
+ // // console.log(tableau);
+
+ //calculer le nombre max//
+ //  function afficheRecommandations(max) {
+ //      return Math.floor(Math.random() *tableau.length;
+ //      console.log(getRandomInt(3));
+ //  }
+ //  console.log(tableau.length);
+
+ //**************teste***********/
+
+ var rand = tableau[Math.floor(Math.random() * tableau.length)];
+ console.log(rand)
+
+ //  const prixMax = 20;
+ //  livres.forEach(function(livre){
+ //         if (livre.prix <= prixMax){
+ //            console.log(livre); 
+ //         } 
+ //  }
